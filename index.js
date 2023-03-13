589d535fba55ff242e0953fa6c9c62dba590a732
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-14 01:38:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+
+let random;
+random = Math.round(Math.random()*10+1);
+console.log(random)
+
+let mod;
+let num3 = 16;
+let num4 = 6;
+mod = num3%num4;
+
+let max;
+max = Math.max(1, 2, 3, 4, 5, 20);
